73eaedabe2ef0480b15790fa157836ea4d9403df
Tzuminn <valuesu0326@gmail.com>
2022-10-15 15:20:38 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
 // 載入express及套件
 const express = require('express')
-const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const methodOverride =  require('method-override')
 
-const Restaurant = require('./models/restaurant')
-
 const routes = require('./routes')
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
-
-// 設定連線到mongoDB
-mongoose.connect(process.env.MONGODB_URI)
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 // 設定為handlebars引擎
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -45,5 +28,4 @@ app.use(routes)
 // 監聽器
 app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`)
-}
-)
+})
